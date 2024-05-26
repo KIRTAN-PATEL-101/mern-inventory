b79a843a965d5a7c5e06d76dbeba15be6e5213bf
PATEL JAY JIGNESHKUMAR <jjpatel24092004@gmail.com>
2024-05-26 19:34:36 +0530

    Error solve successfully

--- Frontend/src/components/SidePanel.jsx
+++ Frontend/src/components/SidePanel.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import 'boxicons/css/boxicons.min.css';
 import { Link } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 
 
 const SidePanel = () => {
   return (
-    <section id="sidebar" className="fixed top-0 left-0 w-72 h-full bg-gray-100 z-50 transition-all overflow-hidden scrollbar-hide">
+    <div>
+      <section id="sidebar" className="fixed top-0 left-0 w-72 h-full bg-gray-100 z-50 transition-all overflow-hidden scrollbar-hide">
       <a href="#" className="brand flex items-center h-14 px-6 text-2xl font-bold text-blue-500 bg-white sticky top-0">
         <span className="text">Store Sync</span>
       </a>
       <ul className="side-menu mt-12">
         <li className="active relative">
-          <Link to="/" className="flex items-center h-12 px-4 text-lg text-gray-800 bg-gray-200 rounded-r-full transition-all">
+            
           <Link to="/" className="flex items-center h-12 px-4 text-lg text-gray-800 bg-gray-200 rounded-r-full transition-all">
             <i className='bx bxs-dashboard flex-shrink-0 text-2xl'></i>
             <span className="ml-4">Dashboard</span>
           </Link>
-          </Link>
+
         </li>
         <li>
           <Link to="/items" className="flex items-center h-12 px-4 text-lg text-gray-800 hover:bg-gray-100 rounded-r-full transition-all">
@@ -26,31 +26,33 @@ const SidePanel = () => {
 
           </Link>
 
-          </Link>
+
         </li>
         <li>
           <Link to="/mystore" className="flex items-center h-12 px-4 text-lg text-gray-800 hover:bg-gray-100 rounded-r-full transition-all">
             <i className='bx bxs-shopping-bag-alt flex-shrink-0 text-2xl'></i>
             <span className="ml-4">My Store</span>
           </Link>
-          </Link>
+
         </li>
         <li>
           <Link to="/settings" className="flex items-center h-12 px-4 text-lg text-gray-800 hover:bg-gray-100 rounded-r-full transition-all">
             <i className='bx bxs-cog flex-shrink-0 text-2xl'></i>
             <span className="ml-4">Settings</span>
           </Link>
-          </Link>
+
         </li>
         <li>
           <Link to="/logut" className="flex items-center h-12 px-4 text-lg text-red-500 hover:bg-gray-100 rounded-r-full transition-all">
             <i className='bx bxs-log-out-circle flex-shrink-0 text-2xl'></i>
             <span className="ml-4">Logout</span>
           </Link>
-          </Link>
+
         </li>
       </ul>
     </section>
+    </div>
+
   );
 };
 
