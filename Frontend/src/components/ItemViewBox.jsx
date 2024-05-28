50f0487bae39fe995335cd1dffb3cda1adb59ff7
Desai2112 <omdesai2112@gmail.com>
2024-05-28 11:30:57 +0530

    View item button fixed

--- Frontend/src/components/ItemViewBox.jsx
+++ Frontend/src/components/ItemViewBox.jsx
@@ -1,67 +1,47 @@
-  import React, { useState } from 'react';
-  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-  import { faTimes } from '@fortawesome/free-solid-svg-icons';
-  import Header from './Header';
-  import SidePanel from './SidePanel';
-  import { useLocation } from 'react-router-dom';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-  const ItemViewBox = () => {
-    const location = useLocation();
-      const item = location.state?.item;
-    const [isVisible, setIsVisible] = useState(false);
+const ItemViewBox = ({ item, onClose }) => {
+  if (!item) return null;
 
-    const toggleVisibility = () => {
-      setIsVisible(!isVisible);
-    };
-
-    return (
-      <div>
-          <main className="flex-1 p-4 ml-64">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={toggleVisibility}
-            >
-              View
-            </button>
-            {isVisible && (
-              <div className="fixed top-16 left-1/2 transform -translate-x-1/2 text-dark w-auto h-auto max-h-[calc(100vh-64px)] z-[99999999] rounded-lg p-4 m-4 bg-white shadow-lg overflow-auto">
-                <div className="flex justify-between">
-                  <h1 className="text-xl font-bold">Item name</h1>
-                  <button onClick={toggleVisibility} className="w-8 h-8">
-                    <FontAwesomeIcon icon={faTimes} />
-                  </button>
-                </div>
-
-                <div className="flex justify-between mt-4">
-                  <div className="p-4 m-3 h-[350px] w-[350px] bg-gray-300 shadow-lg">
-                    <img
-                      src="img/pic3.jpg"
-                      alt={item.itemName}
-                      className="object-cover h-full w-full"
-                    />
-                  </div>
-
-                  <div className="bg-gray-100 p-4 m-3 h-[350px] w-[500px] flex">
-                    <div className="text-gray-700 flex flex-col justify-between w-24">
-                      <span>Name:</span>
-                      <span>Quantity Left:</span>
-                      <span>Category:</span>
-                      <span>Stock</span>
-                    </div>
-                    <div className="flex flex-col justify-between ml-4">
-                      <span>{item.itemName}</span>
-                      <span>{item.stock}</span>
-                      <span>{item.category}</span>
-                      <span className={` px-4 py-2 ${item.stock > 0 ? 'text-green-500' : 'text-red-500'}`}>
-                                        {item.stock > 0 ? 'Yes' : 'No'}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )}
-          </main>
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-[99999999] bg-gray-800 bg-opacity-75">
+      <div className="bg-white rounded-lg p-4 m-4 shadow-lg overflow-auto w-full max-w-4xl">
+        <div className="flex justify-between">
+          <h1 className="text-xl font-bold">{item.itemName}</h1>
+          <button onClick={onClose} className="w-8 h-8">
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </div>
+        <div className="flex justify-between mt-4">
+          <div className="p-4 m-3 h-[350px] w-[350px] bg-gray-300 shadow-lg">
+            <img
+              src="img/pic3.jpg"
+              alt={item.itemName}
+              className="object-cover h-full w-full"
+            />
+          </div>
+          <div className="bg-gray-100 p-4 m-3 h-[350px] w-[500px] flex">
+            <div className="text-gray-700 flex flex-col justify-between w-24">
+              <span>Name:</span>
+              <span>Quantity Left:</span>
+              <span>Category:</span>
+              <span>Stock</span>
+            </div>
+            <div className="flex flex-col justify-between ml-4">
+              <span>{item.itemName}</span>
+              <span>{item.stock}</span>
+              <span>{item.category}</span>
+              <span className={` px-4 py-2 ${item.stock > 0 ? 'text-green-500' : 'text-red-500'}`}>
+                {item.stock > 0 ? 'Yes' : 'No'}
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default ItemViewBox;
+export default ItemViewBox;
