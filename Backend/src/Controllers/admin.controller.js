d15b5e93b9d5007eecf1645a8c1bcb86fe2f0749
Dhaval Agnani <dhavalagnani2684@gmail.com>
2024-05-29 17:52:36 +0530

    Added routes of super admin

---
+++ Backend/src/Controllers/admin.controller.js
@@ -0,0 +1,60 @@
+import { asyncHandler } from "../Utils/asyncHandler.js";
+import { ApiError } from "../Utils/ApiError.js";
+import { ApiResponse } from "../Utils/ApiResponse.js"
+import { User } from "../models/users.models.js";
+import { Inventory } from "../models/inventory.models.js";
+
+const showAllUsers = asyncHandler(async(req, res) =>{
+    const users = await User.find(role = 'user').select("-password");
+    if(!users.length){
+        throw new ApiError(404, "No User Found")
+    };
+    
+});
+const showAllInventories = asyncHandler(async (req, res) => {
+    const inventories = await Inventory.find().select("inventoryId managerName inventoryName address createdAt")
+    if (!inventories.length) {
+      throw new ApiResponse(404, "Inventory not found");
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, inventories, "Inventory list retrieved."));
+});
+const fetchInventoryByUserId = asyncHandler(async(req, res) =>{
+  const UserID = req.user._id;
+  try {
+    const inventory = await Inventory.find({ UserID: UserID });
+    if (!inventory.length) {
+      throw new ApiResponse(404, "Inventory not found");
+    }
+  } catch (error) {
+    console.error("Error fetching the existing Inventory:", error);
+
+  }
+});
+
+const fetchItemsByInventoryId = asyncHandler(async (req, res) => {
+  try {
+    const { inventoryId } = req.body; // Assuming inventoryId is passed as a URL parameter
+    try {
+      // Fetch items from the database by inventoryId
+      const items = await items.find({ inventoryId:inventoryId });
+
+      // Check if items are found
+      if (!items.length) {
+        return res
+          .status(404)
+          .json({ message: "No items found for this inventory." });
+      }
+
+      // Return the items
+      res.status(200).json(items);
+    } catch (error) {
+      // Handle any errors that occur
+      res.status(500).json({ message: error.message });
+    }
+  } catch (error) {
+    throw new ApiError(500, "Bad Request.");
+  }
+});
+export {showAllInventories,  showAllUsers, fetchInventoryByUserId, fetchItemsByInventoryId}
---
+++ Backend/src/Routes/superAdmin.route.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import { verifyJWT } from '../Middlewares/auth.middleware.js'
+import { checkAdmin } from '../Middlewares/checkAdmin.middleware.js';
+import { fetchInventoryByUserId, fetchItemsByInventoryId, showAllInventories, showAllUsers } from '../Controllers/admin.controller.js';
+
+const router=Router();
+
+
+router.route('/inventory').get(verifyJWT,checkAdmin, showAllInventories);
+router.route('/users').get(verifyJWT,checkAdmin, showAllUsers);
+router.route('/users/fetchinventory').get(verifyJWT,checkAdmin, showAllUsers, fetchInventoryByUserId);
+router.route('/users/fetchinventory/fetchitems').get(verifyJWT,checkAdmin, showAllUsers, fetchInventoryByUserId,fetchItemsByInventoryId);
+
+export default router;
