e1bf29298179bec029ea8ac4994acccacc8255a5
Kirtan Patel <kirtan251608@gmail.com>
2024-05-30 12:33:31 +0530

    errors solved

--- Frontend/src/components/PrivateRoutes.jsx
+++ Frontend/src/components/PrivateRoutes.jsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 // import Cookies from "js-cookie";
-import {Cookies} from "react-cookie";
+import { Cookies } from "react-cookie";
 import { useCookies } from "react-cookie";
+import axios from "axios";
 
 const PrivateRoutes = () => {
+  const [islogedIn, setIslogedIn] = useState("0");
 
-   const [cookies, setCookie, removeCookie] = useCookies(['RefreshToken']);
+  useEffect(() => {
+    console.log("hi");
+    axios
+      .get("http://localhost:8000/users/details", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        const data = res.data;
+        // console.log("true");
+        setIslogedIn("1");
+      })
+      .catch((err) => {
+        console.log(err);
+        setIslogedIn("0");
+      });
+    console.log(islogedIn);
+  }, []);
 
-   useEffect(() => {
-      console.log(cookies);
-   }
-
-)
-
-  let auth = { token: true };
-  return (auth.token ? <Outlet /> : <Navigate to="/login" />)
+  return <>{islogedIn === "1" ? <Outlet /> : <Outlet/>}</>;
 };
 
 export default PrivateRoutes;
