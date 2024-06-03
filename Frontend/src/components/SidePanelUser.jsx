4bb60bff77dbefb47a8a6da41163ddf70947f19e
Kirtan Patel <kirtan251608@gmail.com>
2024-06-03 16:35:25 +0530

    Auto login and logout functionality implemented

--- Frontend/src/components/SidePanelUser.jsx
+++ Frontend/src/components/SidePanelUser.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const SidePanel = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,15 +9,28 @@ const SidePanel = ({ children }) => {
     setIsOpen(!isOpen);
   };
 
+  const navigate = useNavigate();
+
   const menuItems = [
     { path: '/dashboard', name: 'Dashboard', icon: 'bx bxs-dashboard' },
-    { path: '/items', name: 'All Items', icon: 'bx bxs-box' },
-    { path: '/inventory', name: 'My Inventories', icon: 'bx bxs-store' },
+    { path: '/items', name: 'All Items', icon: 'bx bxs-box'},
+    { path: '/inventory', name: 'My Inventories', icon: 'bx bxs-store'},
    //  { path: '/geolocation', name: 'Geolocation', icon: 'bx bxs-map'},
     // Add more menu items as needed
     // {path: '/payment', name: 'Payment', icon: 'bx bx-credit-card'},
-    { path: '/login', name: 'Logout', icon: 'bx bxs-log-out-circle' }
+    { path: '/login', name: 'Logout', icon: 'bx bxs-log-out-circle'}
   ];
+
+  const onLogut = () => {
+    axios.get('http://localhost:8000/users/logout', {withCredentials: true})
+    .then((response) => {
+      // localStorage.clear();
+      navigate("/login");
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
 
   return (
     <div className="fixed top-0 left-0 w-72 h-full bg-gray-100 z-50 transition-all overflow-hidden scrollbar-hide">
@@ -36,6 +50,7 @@ const SidePanel = ({ children }) => {
                   isActive ? 'text-gray-800 bg-gray-200' : 'text-gray-800 hover:bg-gray-100'
                 } rounded-r-full transition-all`
               }
+              onClick={item.name === 'Logout' ? onLogut : null}
             >
               <i className={`${item.icon} flex-shrink-0 text-2xl`} aria-label={`${item.name} Icon`}></i>
               <span className="ml-4">{item.name}</span>
