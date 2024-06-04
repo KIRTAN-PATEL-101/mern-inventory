85b627e8c0c3df3be6eafa0269b1311cebbd94df
22IT090-Ansh <22it090@charusat.edu.in>
2024-06-04 11:10:14 +0530

    Map

--- Frontend/src/components/SidePanel.jsx
+++ Frontend/src/components/SidePanel.jsx
@@ -1,5 +1,8 @@
+
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const SidePanel = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -7,21 +10,28 @@ const SidePanel = ({ children }) => {
   const toggle = () => {
     setIsOpen(!isOpen);
   };
-  
 
   const menuItems = [
     { path: '/superAdmin/dashboard', name: 'Dashboard', icon: 'bx bxs-dashboard' },
     { path: '/superAdmin/users', name: 'Users', icon: 'bx bxs-receipt' },
     { path: '/superAdmin/inventory', name: 'All Inventory', icon: 'bx bxs-store' },
-    { path: '/superAdmin/geolocation', name: 'Geolocation', icon: 'bx bxs-map' },
+    { path: '/superAdmin/geolocation', name: 'Geolocation', icon: 'bx bxs-map'},
     // Add more menu items as needed
     {path: '/payment', name: 'Payment', icon: 'bx bx-credit-card'},
-    { path: '/login', name: 'Logout', icon: 'bx bxs-log-out-circle',onclick: 'handleLogout'}
+    { path: '/login', name: 'Logout', icon: 'bx bxs-log-out-circle' }
   ];
 
   const onLogut = () => {
-    localStorage.clear();
-  }
+    axios.get('http://localhost:8000/users/logout', {withCredentials: true})
+  .then((response) => {
+    console.log(response);
+     localStorage.clear();
+    navigate("/login");
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
 
   return (
     <div className="fixed top-0 left-0 w-72 h-full bg-gray-100 z-50 transition-all overflow-hidden scrollbar-hide">
@@ -44,7 +54,7 @@ const SidePanel = ({ children }) => {
             >
               <i className={`${item.icon} flex-shrink-0 text-2xl`} aria-label={`${item.name} Icon`}></i>
               <span className="ml-4">{item.name}</span>
-              {item.name === 'Logout' ?  onLogut() : null}
+              {/* {item.name === 'Logout' ?  onLogut() : null} */}
             </NavLink>
           </li>
         ))}
